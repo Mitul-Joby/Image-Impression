90a8cd086e2079e3134cfed57f3523a73573105d
Mitul Joby <mitul.joby@gmail.com>
2022-03-17 08:47:55 +0530

    Formatting

--- imageGen.py
+++ imageGen.py
@@ -3,6 +3,7 @@ from numpy import uint8
 from imageio import imwrite
 from random import randrange, seed
 
+
 def createSquareMatrix(n):
     matrix = []
     for row in range(n):
@@ -10,9 +11,10 @@ def createSquareMatrix(n):
         for column in range(n):
             randomRGB = []
             for _ in range(3):
-                randomRGB.append(uint8(randrange(0,255)))
+                randomRGB.append(uint8(randrange(0, 255)))
             matrix[row].append(randomRGB)
     return matrix
+
 
 class Matrix:
     def __init__(self, size):
@@ -23,12 +25,14 @@ class Matrix:
         return self.matrix
 
     def toImage(self):
-        imwrite(f'./images/{self.size}x{self.size}.jpg',self.matrix)
-        imwrite(f'./images/{self.size}x{self.size}.jpeg',self.matrix)
-        imwrite(f'./images/{self.size}x{self.size}.png',self.matrix)
-        imwrite(f'./images/{self.size}x{self.size}.tiff',self.matrix)
+        imwrite(f'./images/{self.size}x{self.size}.jpg', self.matrix)
+        imwrite(f'./images/{self.size}x{self.size}.jpeg', self.matrix)
+        imwrite(f'./images/{self.size}x{self.size}.png', self.matrix)
+        imwrite(f'./images/{self.size}x{self.size}.tiff', self.matrix)
+        imwrite(f'./images/{self.size}x{self.size}.bmp', self.matrix)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     seed(time())
     size = int(input("Enter number of pixels: "))
     matrix = Matrix(size)
